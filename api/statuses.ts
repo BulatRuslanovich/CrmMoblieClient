5a1edb08992a6774d00123ff2d854a570621a08d
bulatruslanovich <bulatruslanovich@gmail.com>
2026-03-25 11:52:03 +0300

    feat: delete api status

--- api/statuses.ts
+++
@@ -1,6 +0,0 @@
-import { apiClient } from './client';
-import type { StatusResponse } from './types';
-
-export const statusesApi = {
-  getAll: () => apiClient.get<StatusResponse[]>('/api/statuses'),
-};
